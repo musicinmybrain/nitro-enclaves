85640343988b3d4b6aeed8d2bb415e2b710b932f
Tyler Fanelli <tfanelli@redhat.com>
2026-02-17 00:56:55 -0500

    build: Check for /dev/nitro_enclaves for host flag
    The correct device file is /dev/nitro_enclaves, rather than
    /dev/nitro-enclaves.
    
    Signed-off-by: Tyler Fanelli <tfanelli@redhat.com>

--- build.rs
+++ build.rs
@@ -5,7 +5,7 @@ use std::path::Path;
 fn main() {
     println!("cargo::rustc-check-cfg=cfg(host)");
 
-    if Path::new("/dev/nitro-enclaves").exists() {
+    if Path::new("/dev/nitro_enclaves").exists() {
         println!("cargo:rustc-cfg=host");
     }
 }
